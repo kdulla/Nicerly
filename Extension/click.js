8942df434c78597a672503b92fc5aae7494de4d5
kdulla <43546681+kdulla@users.noreply.github.com>
2019-02-09 13:49:43 -0500

    Add files via upload

--- Extension/click.js
+++ Extension/click.js
@@ -1,21 +1,72 @@
 console.log("HELLO WORLD")
+
 setInterval(function(){
-	 var temp = document.getElementsByClassName("_1mf _1mj")[0];
-	 if (temp != null){
-	 var tex = temp.children[0].textContent;
+	var temp = document.getElementsByClassName("_1mf _1mj")[0];
+	if (temp != null) {
+		var tex = "";
+		for (i = 0; i < temp.children.length; i++) { 
+			tex += ignoreEmojies(temp.children[i]);
+		}
+		
+		}
+		if (tex != null) {
+		
+		getNiceness(tex, draw_box);
+		}
+
 	
-	 if (tex != null) {
-		 console.log(tex);
-		 if (document.getElementById("added") == null){
-			var div = document.createElement("div");
-			div.id = "added"
-		 }
-		 else{
-			 var div = document.getElementById("added");
-		 }
-		 div.className = "speech-bubble";
-		 div.innerHTML = tex;
-		 document.body.appendChild(div);
-		 }
+}, 400);
+
+function ignoreEmojies(object){
+	if (object.className != null && object.className != "_21wj"){
+		return object.textContent;
 	}
-}, 4000);
+}
+
+function draw_box(value) {
+	var message = "";
+	if (value < -30){
+		message = "Wow, you seem to be going through alot of effort to be mean here, but CYBERBULLYING IS BAD";
+	}
+	else if (value < -15){
+		message = "That seems mean. I really don't think you should send that.";
+	}
+	else if (value < -5){
+		message = "Hey, that seems mean, how would you feel if you received that?";
+	}
+	else if (value < -1){
+		message = "Hey, that doesn't seem very nice!";
+	}
+	if (document.getElementById("added") == null){
+		var div = document.createElement("div");
+		div.id = "added"
+	}
+	else{
+		var div = document.getElementById("added");
+		}
+	div.className = "speech-bubble";
+	div.innerHTML = message;
+	var coords = document.getElementsByClassName("_1mf _1mj")[0].children[document.getElementsByClassName("_1mf _1mj")[0].children.length - 1].getBoundingClientRect();
+	div.style.top = Math.round(coords.bottom - 45).toString() + ".px";
+	div.style.left = Math.round(coords.right + 20).toString() + ".px";
+	if (message == ""){
+	 div.style.display = "none";
+	}
+	else{
+	div.style.display = "block";
+	}
+	document.body.appendChild(div);
+}
+
+function getNiceness(message, callback) {
+	const Http = new XMLHttpRequest();
+	const url='https://nicerly.azurewebsites.net/analyze?message=' + message;
+	Http.open("GET", url);
+	Http.send();
+	Http.onreadystatechange = (e) => {
+		if (Http.responseText.length != 0) {
+			callback(Http.responseText)
+		}
+	}
+}
+
