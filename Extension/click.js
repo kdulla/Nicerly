e2478df2d2ad7994f2f719bcd603278ad47f4bb0
Antioch Sanders <antiochsanders@gmail.com>
2019-02-09 01:37:06 -0500

    Add web requests

--- Extension/click.js
+++ Extension/click.js
@@ -1,16 +1,24 @@
 console.log("HELLO WORLD")
 setInterval(function(){
-	 var temp = document.getElementsByClassName("_1mf _1mj")[0];
-	 if (temp != null){
-	 var tex = temp.children[0].textContent;
-	
-	 if (tex != null) {console.log(tex)};}
-}, 4000);
+	var temp = document.getElementsByClassName("_1mf _1mj")[0];
+	if (temp != null) {
+		var tex = temp.children[0].textContent;
 
-const Http = new XMLHttpRequest();
-const url='https://nicerly.azurewebsites.net/analyze?message=';
-Http.open("GET", url);
-Http.send();
-Http.onreadystatechange=(e)=>{
-console.log(Http.responseText)
+		if (tex != null) {
+			getNiceness(tex);
+		}
+	}
+}, 400);
+
+function getNiceness(message) {
+	const Http = new XMLHttpRequest();
+	const url='https://nicerly.azurewebsites.net/analyze?message=' + message;
+	Http.open("GET", url);
+	Http.send();
+	Http.onreadystatechange = (e) => {
+		if (Http.responseText.length != 0) {
+			console.log(Http.responseText)
+		}
+	}
 }
+
